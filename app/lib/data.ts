f78d086d8f5e08aa5304f1e6b132b54ae4fc7b2b
Thanoraj <muthulingamthanoraj@gmail.com>
2025-01-15 19:23:50 +0530

    intro section added

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -29,3 +29,13 @@ export const routes = [
     hash: "#contact",
   },
 ] as const;
+
+export const introData = {
+  p1: "Hi, I'm Thanoraj!",
+  p2: "I'm a passionate ",
+  p3: "AI & Full stack Engineer ",
+  p4: "with ",
+  p5: "3 years ",
+  p6: "of experience in development. I thrive on crafting intuitive and engaging user experiences and building scalable, high-performing applications. With a strong foundation in ",
+  p7: "React, Next.js, Flutter and Python.",
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import IntroSection from "./components/intro-section";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="pt-28 sm:pt-36 flex flex-col items-center">
+      <IntroSection />
+    </main>
+  );
 }
